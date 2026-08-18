452ac37c4d7576ea04ebc0445ad07cc611bfda36
Andhika Rahman <andhikarhmnp@gmail.com>
2026-08-18 16:22:17 +0700

    Refactoring the codebase

---
+++ src/app_status.rs
@@ -0,0 +1,2 @@
+pub const APP_RUNNING: u32 = 100;
+pub const APP_STOPPED: u32 = 200;
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-const APP_RUNNING: u8 = 100;
-const APP_STOPPED: u8 = 200;
-
-fn app_init() {
-    let logo = r#"
-     _       ________________  ____ 
-    | |     / / ____/  _/ __ \/ __ \
-    | | /| / / __/  / // /_/ / / / /
-    | |/ |/ / /____/ // _, _/ /_/ / 
-    |__/|__/_____/___/_/ |_/_____/  
-                                    
-       _________    __    ________  ____    ___  __________  ____ 
-      / ____/   |  / /   / ____/ / / / /   /   |/_  __/ __ \/ __ \
-     / /   / /| | / /   / /   / /   / /   / /| | / / / / / / /_/ /
-    / /___/ ___ |/ /___/ /___/ /_/ / /___/ ___ |/ / / /_/ / _, _/ 
-    \____/_/  |_/_____/\____/\____/_____/_/  |_/_/  \____/_/ |_| 
-    "#;
-
-    println!("{}", logo);
-
-    println!("Weird Calculator v0.1 - by Andhika Rahman");
-    println!("Type ‘HELP’ to see all available commands.");
-}
+mod app_init;
+mod app_status;
 
 fn main() {
-    let mut app_status = APP_RUNNING;
+    let mut app_status = app_status::APP_RUNNING;
 
-    app_init();
+    app_init::app_init();
 }
