27ffe7a86d46696b87f955d572f93d04afc48ce9
Andhika Rahman <andhikarhmnp@gmail.com>
2026-08-18 22:46:05 +0700

    Coloring the app_init component

--- src/main.rs
+++ src/main.rs
@@ -209,24 +209,55 @@ fn exec_cmd(cmd: &str) -> u32 {
 }
 
 fn app_init() {
-    let logo = r#"
-     _       ________________  ____ 
-    | |     / / ____/  _/ __ \/ __ \
-    | | /| / / __/  / // /_/ / / / /
-    | |/ |/ / /____/ // _, _/ /_/ / 
-    |__/|__/_____/___/_/ |_/_____/  
-                                    
-       _________    __    ________  ____    ___  __________  ____ 
-      / ____/   |  / /   / ____/ / / / /   /   |/_  __/ __ \/ __ \
-     / /   / /| | / /   / /   / /   / /   / /| | / / / / / / /_/ /
-    / /___/ ___ |/ /___/ /___/ /_/ / /___/ ___ |/ / / /_/ / _, _/ 
-    \____/_/  |_/_____/\____/\____/_____/_/  |_/_/  \____/_/ |_|  
-    "#;
+    // Logo dipecah per baris untuk efek gradien
+    let logo_lines = [
+        r#"     _       ________________  ____ "#,
+        r#"    | |     / / ____/  _/ __ \/ __ \"#,
+        r#"    | | /| / / __/  / // /_/ / / / /"#,
+        r#"    | |/ |/ / /____/ // _, _/ /_/ / "#,
+        r#"    |__/|__/_____/___/_/ |_/_____/  "#,
+        r#"                                    "#,
+        r#"        _________    __    ________  ____    ___  __________  ____ "#,
+        r#"       / ____/   |  / /   / ____/ / / / /   /   |/_  __/ __ \/ __ \"#,
+        r#"      / /   / /| | / /   / /   / /   / /   / /| | / / / / / / /_/ /"#,
+        r#"     / /___/ ___ |/ /___/ /___/ /_/ / /___/ ___ |/ / / /_/ / _, _/ "#,
+        r#"     \____/_/  |_/_____/\____/\____/_____/_/  |_/_/  \____/_/ |_|  "#,
+    ];
 
-    println!("{}", logo);
+    // Array warna untuk membuat gradien Magenta -> Cyan
+    let colors = [
+        (255, 0, 255),
+        (220, 30, 255),
+        (180, 60, 255),
+        (140, 90, 255),
+        (100, 120, 255),
+        (80, 150, 255),
+        (60, 180, 255),
+        (40, 210, 255),
+        (20, 230, 255),
+        (0, 255, 255),
+        (0, 255, 255),
+    ];
+
+    println!();
     
-    println!("Weird Calculator v0.1 - by Andhika Rahman");
-    println!("Type \'HELP\' to see all available commands.");
+    // Print logo dengan efek gradien TrueColor
+    for (line, (r, g, b)) in logo_lines.iter().zip(colors.iter()) {
+        println!("{}", line.truecolor(*r, *g, *b).bold());
+    }
+
+    println!(
+        " {} {} {} {}",
+        " Weird Calculator ".on_custom_color(CustomColor::new(40, 40, 40)).bold(),
+        "v0.1".yellow().bold(),
+        "• by".dimmed(),
+        "Andhika Rahman".green().bold()
+    );
+
+    println!(
+        " Type '{}' to see all available commands.\n",
+        "HELP".cyan().bold()
+    );
 }
 
 fn main() {
